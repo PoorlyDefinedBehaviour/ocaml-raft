3b9298246c23efb83f0b49007efed3b530d87393
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2024-01-21 23:46:17 -0300

    encode and decode request_vote_output, append_entries_input,  append_entries_output

--- lib/tcp_transport.ml
+++ lib/tcp_transport.ml
@@ -11,6 +11,9 @@ type t = {
 }
 
 let message_type_request_vote_input = '1'
+let message_type_request_vote_output = '2'
+let message_type_append_entries_input = '3'
+let message_type_append_entries_output = '4'
 
 type config = {
   cluster_members : (Protocol.replica_id, Eio.Net.Sockaddr.stream) Hashtbl.t;
@@ -19,6 +22,18 @@ type config = {
 type string_reader = { position : int ref; buffer : string }
 
 let create_string_reader (buffer : string) = { position = ref 0; buffer }
+
+let read_bool (reader : string_reader) : bool =
+  let value = String.get reader.buffer !(reader.position) in
+  reader.position := !(reader.position) + 1;
+  match value with
+  | 't' -> true
+  | 'f' -> false
+  | c ->
+      raise
+        (Invalid_argument
+           (Printf.sprintf
+              "read_bool: expected a char representing a boolean but got: %c" c))
 
 let read_char (reader : string_reader) =
   let value = String.get reader.buffer !(reader.position) in
@@ -34,6 +49,15 @@ let read_int32_be (reader : string_reader) =
   let value = String.get_int32_be reader.buffer !(reader.position) in
   reader.position := !(reader.position) + 4;
   value
+
+let read_string_of_len (reader : string_reader) (len : int64) : string =
+  let data_len = Int64.to_int len in
+  let buffer = Bytes.make data_len '0' in
+  String.blit reader.buffer !(reader.position) buffer 0 data_len;
+  reader.position := !(reader.position) + data_len;
+  Bytes.to_string buffer
+
+let encode_bool (b : bool) : char = if b then 't' else 'f'
 
 let encode_request_vote_input (message : Protocol.request_vote_input) =
   let buffer = Buffer.create 0 in
@@ -74,16 +98,132 @@ let%test_unit "encode - decode request vote input" =
 
 let%test_unit "quickcheck: encode - decode request vote input" =
   let test =
-    QCheck.Test.make ~count:1000 ~name:"list_rev_is_involutive"
+    QCheck.Test.make ~count:1000 ~name:"basic"
       (QCheck.make Protocol.gen_request_vote_input) (fun message ->
         encode_request_vote_input message |> decode_request_vote_input = message)
   in
   QCheck.Test.check_exn test
 
-(* TODO: continue implementing the tcp transport*)
-let encode_request_vote_output message = assert false
-let encode_append_entries_input message = assert false
-let encode_append_entries_output mesage = assert false
+let encode_request_vote_output (message : Protocol.request_vote_output) =
+  let buffer = Buffer.create 0 in
+  Buffer.add_char buffer message_type_request_vote_output;
+  Buffer.add_int64_be buffer message.term;
+  Buffer.add_int32_be buffer message.replica_id;
+  Buffer.add_char buffer (encode_bool message.vote_granted);
+  Buffer.contents buffer
+
+let decode_request_vote_output (buffer : string) : Protocol.request_vote_output
+    =
+  let reader = create_string_reader buffer in
+
+  let message_type = read_char reader in
+  assert (message_type = message_type_request_vote_output);
+
+  let term = read_int64_be reader in
+  let replica_id = read_int32_be reader in
+  let vote_granted = read_bool reader in
+  { term; replica_id; vote_granted }
+
+let%test_unit "quickcheck: encode - decode request vote output" =
+  let test =
+    QCheck.Test.make ~count:1000 ~name:"basic"
+      (QCheck.make Protocol.gen_request_vote_output) (fun message ->
+        encode_request_vote_output message
+        |> decode_request_vote_output = message)
+  in
+  QCheck.Test.check_exn test
+
+let encode_entry (buffer : Buffer.t) (entry : Protocol.entry) : unit =
+  Buffer.add_int64_be buffer entry.term;
+  Buffer.add_int64_be buffer (Int64.of_int (String.length entry.data));
+  Buffer.add_string buffer entry.data
+
+let read_entry (reader : string_reader) : Protocol.entry =
+  let term = read_int64_be reader in
+  let data_len = read_int64_be reader in
+  let data = read_string_of_len reader data_len in
+  { term; data }
+
+let read_entries (reader : string_reader) : Protocol.entry list =
+  let num_entries = read_int64_be reader in
+  List.init (Int64.to_int num_entries) (fun _ -> read_entry reader)
+
+let encode_append_entries_input (message : Protocol.append_entries_input) =
+  let buffer = Buffer.create 0 in
+  Buffer.add_char buffer message_type_append_entries_input;
+  Buffer.add_int64_be buffer message.leader_term;
+  Buffer.add_int32_be buffer message.leader_id;
+  Buffer.add_int32_be buffer message.replica_id;
+  Buffer.add_int64_be buffer message.previous_log_index;
+  Buffer.add_int64_be buffer message.previous_log_term;
+  Buffer.add_int64_be buffer (Int64.of_int (List.length message.entries));
+  List.iter (fun entry -> encode_entry buffer entry) message.entries;
+  Buffer.add_int64_be buffer message.leader_commit;
+  Buffer.contents buffer
+
+let decode_append_entries_input (buffer : string) :
+    Protocol.append_entries_input =
+  let reader = create_string_reader buffer in
+
+  let message_type = read_char reader in
+  assert (message_type = message_type_append_entries_input);
+
+  let leader_term = read_int64_be reader in
+  let leader_id = read_int32_be reader in
+  let replica_id = read_int32_be reader in
+  let previous_log_index = read_int64_be reader in
+  let previous_log_term = read_int64_be reader in
+  let entries = read_entries reader in
+  let leader_commit = read_int64_be reader in
+  {
+    leader_term;
+    leader_id;
+    replica_id;
+    previous_log_index;
+    previous_log_term;
+    entries;
+    leader_commit;
+  }
+
+let%test_unit "quickcheck: encode - decode append entries input" =
+  let test =
+    QCheck.Test.make ~count:1000 ~name:"basic"
+      (QCheck.make Protocol.gen_append_entries_input) (fun message ->
+        encode_append_entries_input message
+        |> decode_append_entries_input = message)
+  in
+  QCheck.Test.check_exn test
+
+let encode_append_entries_output (message : Protocol.append_entries_output) =
+  let buffer = Buffer.create 0 in
+  Buffer.add_char buffer message_type_append_entries_output;
+  Buffer.add_int64_be buffer message.term;
+  Buffer.add_char buffer (encode_bool message.success);
+  Buffer.add_int64_be buffer message.last_log_index;
+  Buffer.add_int32_be buffer message.replica_id;
+  Buffer.contents buffer
+
+let decode_append_entries_output (buffer : string) :
+    Protocol.append_entries_output =
+  let reader = create_string_reader buffer in
+
+  let message_type = read_char reader in
+  assert (message_type = message_type_append_entries_output);
+
+  let term = read_int64_be reader in
+  let success = read_bool reader in
+  let last_log_index = read_int64_be reader in
+  let replica_id = read_int32_be reader in
+  { term; success; last_log_index; replica_id }
+
+let%test_unit "quickcheck: encode - decode append entries output" =
+  let test =
+    QCheck.Test.make ~count:1000 ~name:"basic"
+      (QCheck.make Protocol.gen_append_entries_output) (fun message ->
+        encode_append_entries_output message
+        |> decode_append_entries_output = message)
+  in
+  QCheck.Test.check_exn test
 
 (* Creates a connection if a connection does not already exist in the connection map.
    Returns the connection. *)

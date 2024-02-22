5ec656c4e0573a112ea1b9470b6b478b166e74e5
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2024-02-21 23:58:45 -0300

    wip: send request from client to replica

--- bin/client.ml
+++ bin/client.ml
@@ -14,10 +14,32 @@ let connect_to_any_server ~sw ~net ~server_addresses =
     (fun addr -> try Some (Eio.Net.connect ~sw net addr) with _ -> None)
     server_addresses
 
-let main ~net =
+let main ~net ~stdin =
   Eio.Switch.run @@ fun sw ->
-  match connect_to_any_server ~sw ~net ~server_addresses with
-  | None -> traceln "unable to connect to a server"
-  | Some conn -> traceln "connected to server"
+  let rec loop () =
+    let line = Eio.Buf_read.line stdin in
+    let parts = String.split_on_char ' ' line in
+    let cmd = List.nth parts 0 in
+    match cmd with
+    | "SET" ->
+        let key = List.nth parts 1 in
+        let value = List.nth parts 2 in
+        let encoded = Raft.Kv.encode_set_command key value in
+        (* TODO: connect to a replica and send request *)
+        assert false
+    | "GET" -> assert false
+    | _ ->
+        raise
+          (Invalid_argument
+             (Format.sprintf "unknown command: %s in %s" cmd line))
+  in
 
-let () = Eio_main.run (fun env -> main ~net:(Eio.Stdenv.net env))
+  loop ()
+(* match connect_to_any_server ~sw ~net ~server_addresses with
+   | None -> traceln "unable to connect to a server"
+   | Some conn -> traceln "connected to server" *)
+
+let () =
+  Eio_main.run (fun env ->
+      main ~net:(Eio.Stdenv.net env)
+        ~stdin:(Eio.Buf_read.of_flow ~max_size:1024 (Eio.Stdenv.stdin env)))
--- lib/kv.ml
+++ lib/kv.ml
@@ -1,14 +1,27 @@
 type t = { entries : (string, string) Hashtbl.t }
+type set_command = { key : string; value : string } [@@deriving show]
+
+(* TODO: test encode and decode *)
+let encode_set_command (key : string) (value : string) : string =
+  let buffer = Buffer.create 0 in
+  Buffer.add_int32_be buffer (String.length key |> Int32.of_int);
+  Buffer.add_string buffer key;
+  Buffer.add_int32_be buffer (String.length value |> Int32.of_int);
+  Buffer.add_string buffer value;
+  Buffer.contents buffer
+
+let decode_set_command (buffer : string) : set_command =
+  let key_len = Int32.to_int (String.get_int32_be buffer 0) in
+  let key = String.sub buffer 4 key_len in
+  let value_len = Int32.to_int (String.get_int32_be buffer (4 + key_len)) in
+  let value = String.sub buffer (4 + key_len + 4) value_len in
+  { key; value }
 
 let create () : t = { entries = Hashtbl.create 0 }
 
 let apply (kv : t) (entry : Protocol.entry) : unit =
-  let key_len = Int32.to_int (String.get_int32_be entry.data 0) in
-  let key = String.sub entry.data 4 key_len in
-  let value_len = Int32.to_int (String.get_int32_be entry.data (4 + key_len)) in
-  let value = String.sub entry.data (4 + key_len + 4) value_len in
-
-  Hashtbl.replace kv.entries key value
+  let command = decode_set_command entry.data in
+  Hashtbl.replace kv.entries command.key command.value
 
 let%test_unit "apply" =
   let kv = create () in
